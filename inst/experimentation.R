69bdd385c7425a28dd8109483fe7a59998aaebd0
Gustav Delius <gustav.delius@gmail.com>
2023-12-26 22:53:39 +0100

    First working version, still untested.

--- inst/experimentation.R
+++ inst/experimentation.R
@@ -1,17 +1,19 @@
 p <- newTraitParams()
 p <- setBevertonHolt(p, reproduction_level = 0)
 p <- steady(p)
-p@species_params$sr_r0 <- 100
-p@species_params$sr_exp <- 1e4
+p@species_params$sr_r0 <- 10
+p@species_params$sr_exp <- 1e3
 p@species_params$sr_t0 <- 0.2
 p <- setSeasonalReproduction(p)
 
-sim <- project(p, t_max = 3, t_save = 0.02, dt = 0.01)
+sim <- project(p, t_max = 10, t_save = 0.05, dt = 0.01)
+p <- setInitialValues(p, sim)
 
 plotRDI(sim, log = TRUE)
+plotRDI(sim, log = FALSE)
 plotRDD(sim, log = TRUE)
 plotSpectra(sim)
-animateSpectra(sim, power = 2)
+animateSpectra(sim, power = 2, time_range = c(9,10))
 animateGonadSpectra(sim)
 
 t <- seq(0, 1, by = 0.01)
